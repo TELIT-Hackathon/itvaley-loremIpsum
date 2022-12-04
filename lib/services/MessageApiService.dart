3b3551690e8dfb4e02aa926d87288cf64d9228e5
Oleksandr <oleksandr.smahlii@student.tuke.sk>
2022-12-04 02:34:25 +0100

    Banned message, Status, Type API

--- lib/services/MessageApiService.dart
+++ lib/services/MessageApiService.dart
@@ -1,3 +1,4 @@
+import 'package:it_valey_hackathon_2022/entity/BannedMessage.dart';
 import 'package:it_valey_hackathon_2022/entity/Message.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
@@ -36,6 +37,13 @@ class MessageApiService {
   static Future<int> createNewMessage(Message message) async {
     Uri requestUrl = Uri.parse("https://quiet-brook-94275.herokuapp.com/create_message");
     final http.Response response = await http.post(requestUrl, body: json.encode(message));
+
+    return response.statusCode;
+  }
+
+  static Future<int> createNewBannedMessage(BannedMessage bannedMessage) async {
+    Uri requestUrl = Uri.parse("https://quiet-brook-94275.herokuapp.com/create_banned_messages");
+    final http.Response response = await http.post(requestUrl, body: json.encode(bannedMessage));
 
     return response.statusCode;
   }
