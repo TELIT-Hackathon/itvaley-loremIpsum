ddd0270cafeea4b39f45165db46c0f1cad9f3600
Oleksandr <oleksandr.smahlii@student.tuke.sk>
2022-12-04 03:08:41 +0100

    Topics page widget Setting page widget Profile page widget

---
+++ lib/ui/pages/SettingsPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class SettingsPage extends StatefulWidget{
+  const SettingsPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<StatefulWidget>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
